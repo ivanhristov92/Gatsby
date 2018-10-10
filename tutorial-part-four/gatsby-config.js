999ba64f3ac21c78cdfd4abbcab0717984d8c1cc
ivanhristov92 <ivan_hristov@mail.bg>
2018-10-10 11:08:45 +0300

    deploy

--- tutorial-part-four/gatsby-config.js
+++ tutorial-part-four/gatsby-config.js
@@ -4,6 +4,21 @@ module.exports = {
     },
     plugins: [
         {
+            resolve: `gatsby-plugin-manifest`,
+            options: {
+                name: "GatsbyJS",
+                short_name: "GatsbyJS",
+                start_url: "/",
+                background_color: "#6b37bf",
+                theme_color: "#6b37bf",
+                display: "minimal-ui",
+                icon: "src/images/icon.png", // This path is relative to the root of the site.
+            },
+        },
+        'gatsby-plugin-offline',
+        `gatsby-plugin-react-helmet`,
+
+        {
             resolve: `gatsby-source-filesystem`,
             options: {
                 name: `src`,
